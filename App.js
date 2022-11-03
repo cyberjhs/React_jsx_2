ca63326fca488c7ae95bf0727ce8578958a12d42
Navneet Vishwakarma <68511498+cyberjhs@users.noreply.github.com>
2022-11-03 11:53:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import companyLogo from './logo.png';
+function App() {
+  return (
+    <>
+    <div className="about-Text">
+      <p>The Evolution Of<br/> Airbnb's Frontent</p>
+      <div>
+        <h1>@Spikebrehm</h1><br/>
+        <img src={companyLogo} alt="Airbnb" />
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
